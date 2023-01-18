b83763aa8a04d863e8bdcabfc931bf8aab47dd75
Tullio Sebastiani <tullio.sebastiani@x3solutions.it>
2023-01-18 10:34:15 +0100

    test_pr

--- ssl.sh
+++ ssl.sh
@@ -4,3 +4,4 @@ openssl genrsa -out server.key
 openssl req -new -key server.key -out server_reqout.txt -subj '/CN=nginx.apps.apps.18.236.123.92.nip.io/O=Red Hat Inc./C=US'
 openssl x509 -req -in server_reqout.txt -days 3650 -sha256 -CAcreateserial -CA serverca.crt -CAkey servercakey.pem -out server.crt
 oc create route --namespace test  edge --service=nginx-service --cert=server.crt --key=server.key --ca-cert=serverca.crt --hostname=nginx.apps.apps.18.236.123.92.nip.io
+
